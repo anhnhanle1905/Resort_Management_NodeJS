5e6d6725243ea9251c09d30cd5ccaffe9c744330
anhnhanle1905 <75842089+anhnhanle1905@users.noreply.github.com>
2022-04-09 10:22:14 +0700

    Update controller/auth/logout

--- src/routers/auth.routers.js
+++ src/routers/auth.routers.js
@@ -1,14 +1,17 @@
 import express from "express";
 const router = express.Router();
+import auth from "../middleware/auth.js";
 
 import {
   registerUser,
   loginUser,
+  logOut,
   changePw,
 } from "../controller/auth.controller.js";
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/logOut", auth, logOut);
 router.post("/changePw", changePw);
 
 export default router;
